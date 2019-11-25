ce2e6f693a414b454a19636d343998e188452f5f
Gustavo Espinosa <age.espinosa@gmail.com>
2019-11-25 19:36:23 -0300

    044 Generate fake data in fixtures using Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\Comment;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -16,43 +17,57 @@ class AppFixtures extends Fixture
      */
     private $passwordEncoder;
 
+    /**
+     * @var \Faker\Factory
+     */
+    private $faker;
+
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->faker= \Faker\Factory::create();
     }
 
     public function load(ObjectManager $manager)
     {
         $this->loadUsers($manager);
         $this->loadBlogPosts($manager);
+        $this->loadComments($manager);
     }
 
     public function loadBlogPosts(ObjectManager $manager){
 
         $user=$this->getReference('user_admin');
-        $blogPost= new BlogPost();
-        $blogPost->setContent("Este es el contenido de monica");
-        $blogPost->setPublished(new \DateTime("2019-10-20 12:00:00"));
-        $blogPost->setSlug("blog-moni");
-        $blogPost->setTitle("Religion y mascotas");
-        $blogPost->setAuthor($user);
+        for ($i=0; $i<100; $i++){
+            $blogPost= new BlogPost();
+            $blogPost->setTitle($this->faker->realText(30));
+            $blogPost->setPublished($this->faker->dateTimeThisYear);
+            $blogPost->setContent($this->faker->realText());
+            $blogPost->setSlug("blog-moni");
+            $blogPost->setAuthor($user);
 
-        $manager->persist($blogPost);
+            $this->addReference("blog_post_$i", $blogPost);
 
-        $blogPost= new BlogPost();
-        $blogPost->setContent("Este es el contenido de Rene");
-        $blogPost->setPublished(new \DateTime("2019-10-10 12:00:00"));
-        $blogPost->setSlug("blog-rene");
-        $blogPost->setTitle("Motos y webgl");
-        $blogPost->setAuthor($user);
-
-        $manager->persist($blogPost);
+            $manager->persist($blogPost);
+        }
 
         $manager->flush();
 
     }
     public function loadComments(ObjectManager $manager){
 
+        for ($i=0; $i<100; $i++){
+            for ($j=0, $jMax = rand(1, 10); $j< $jMax; $j++){
+                $comment= new Comment();
+                $comment->setContent($this->faker->realText());
+                $comment->setPublished($this->faker->dateTimeThisYear);
+                $comment->setAuthor($this->getReference("user_admin"));
+
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
     }
     public function loadUsers(ObjectManager $manager){
 
